fcf7d45e260c53a1b93b68b9046a64a5159ef8f7
Dovy Paukstys <info@simplerain.com>
2013-08-10 18:06:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,13 @@ get_header(); ?>
 	<article>
 
 		<header class="entry-header">
-			<h1>SMOF TEST</h1>
+			<h1>Simple Options Framework</h1>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
 
 		<pre>
 		<?php
-		$data_r = print_r($smof_data, true);
-		$data_r_sans = htmlspecialchars($data_r, ENT_QUOTES);
-		echo $data_r_sans;
 
 		?>
 
